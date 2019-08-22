72892a5649e680a653197a44426ea35d72b3257c
Andrew Kinstler <andrewckinstler@gmail.com>
2019-08-22 14:18:42 -0600

    Commit to push

--- main.js
+++ main.js
@@ -4,37 +4,75 @@ var rangeButton = document.getElementById('js__range--button');
 var minNumberDisplay = document.getElementById('min__number--display');
 var maxNumberDisplay = document.getElementById('max__number--display');
 var submitGuess = document.getElementById('guess__submit--button');
+var playerOneGuess = document.getElementById("js__name1--guess")
+var currentGuessOne = document.getElementById("player__one--guess")
+var playerTwoGuess = document.getElementById("js__name2--guess")
+var currentGuessTwo = document.getElementById("player__two--guess")
+var pInner = document.getElementById("p__inner");
+var p2Inner = document.getElementById("p2__inner");
+var min = Math.ceil(minRange.value);
+var max = Math.floor(maxRange.value);
+var randomNumber = Math.floor(Math.random() * (maxRange.value - minRange.value)) + minRange.value;
+
 
 rangeButton.addEventListener('click', setRangeValue);
+submitGuess.addEventListener('click', changeText);
+
 
 function setRangeValue(){
-	setMinValue();
-	setMaxValue();
-	generateNumber();
+    setMinValue();
+    setMaxValue();
 };
 
 function setMinValue(){
-	minNumberDisplay.innerText = minRange.value;
+    minNumberDisplay.innerText = minRange.value;
 };
 
 function setMaxValue(){
-	maxNumberDisplay.innerText = maxRange.value;
+    maxNumberDisplay.innerText = maxRange.value;
 };
-
-function generateNumber(){
-	var min = Math.ceil(minRange.value);
-	var max = Math.floor(maxRange.value);
-	var randomNumber = Math.floor(Math.random() * (max - min)) + min;
-	console.log(randomNumber);
-}
-
+// function generateNumber(){
+// }
 function changeText() {
-		var playerOneGuess = document.getElementById("js__name1--guess")
-		var currentGuessOne = document.getElementById("player__one--guess")
-		currentGuessOne.innerText = playerOneGuess.value;
-		var playerTwoGuess = document.getElementById("js__name2--guess")
-		var currentGuessTwo = document.getElementById("player__two--guess")
-		currentGuessTwo.innerText = playerTwoGuess.value;
+        currentGuessOne.innerText = playerOneGuess.value;
+        currentGuessTwo.innerText = playerTwoGuess.value;
+        winnerP1()
 }
+//
+// function clearForms() {
+//     currentGuessOne.value = '';
+//     currentGuessTwo.value = '';
+// };
 
-submitGuess.addEventListener('click', changeText);
+function winnerP1() {
+	console.log(randomNumber.value);
+if (playerOneGuess.value === randomNumber){
+		pInner.innerText = "BOOM!";
+	}
+    if (playerOneGuess.value > randomNumber) {
+         pInner.innerText = "that's too high";
+    } else {
+            pInner.innerText = "that's too low";
+}
+}
+// function winnerP2() {
+// 	if (player__one.value === randomNumber){
+// 		console.log('winner winner!')
+// 	}
+//   if (playerTwoGuess.value > randomNumber) {
+//          p2Inner.innerText = "that's too high";
+//     } else {
+//          p2Inner.innerText = "that's too low";
+//             clearForms();
+// }
+// }
+
+
+
+
+// function generateNumber(){
+// 	var min = Math.ceil(minRange.value);
+// 	var max = Math.floor(maxRange.value);
+// 	var randomNumber = Math.floor(Math.random() * (max - min)) + min;
+// 	console.log(randomNumber);
+// }
